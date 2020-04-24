6406917d59162ceb6edaeb08594e4c7f4e3a0874
Hannah M.M. Eriksen <hannah_eriksen@hotmail.com>
2020-04-24 11:17:10 +0200

    Initial commit

---
+++ src/test/java/webprogrammeringinnlevering3/packages/PackagesApplicationTests.java
@@ -0,0 +1,13 @@
+package webprogrammeringinnlevering3.packages;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PackagesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
